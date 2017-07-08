4fb0ec2dbb7231fae0853bd3f8c8452c3a86ca19
Akshay Singh <akshaysingh.1713@gmail.com>
2017-07-08 11:43:27 +0530

    Scene2D tut start

--- core/src/com/mygdx/drop/DropGame.java
+++ core/src/com/mygdx/drop/DropGame.java
@@ -1,140 +1,54 @@
 package com.mygdx.drop;
 
-import com.badlogic.gdx.*;
-import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.audio.Sound;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.Texture.TextureFilter;
-import com.badlogic.gdx.graphics.g2d.*;
-import com.badlogic.gdx.input.GestureDetector;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Array;
-import com.badlogic.gdx.utils.TimeUtils;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.utils.Timer;
-import com.badlogic.gdx.utils.Timer.Task;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 
-import java.security.Key;
-import java.util.Iterator;
+public class DropGame implements ApplicationListener {
 
+    public class MyActor extends Actor {
+        Texture texture = new Texture(Gdx.files.internal("jet.png"));
+        @Override
+        public void draw(Batch batch, float alpha){
+            batch.draw(texture,0,0);
+        }
+    }
 
-public class DropGame extends ApplicationAdapter implements InputProcessor, GestureDetector.GestureListener {
-    private OrthographicCamera camera;
-    private SpriteBatch batch;
-    private Texture texture;
-    private Sprite sprite;
+    private Stage stage;
 
     @Override
     public void create() {
-        camera = new OrthographicCamera(1280, 720);
-        batch = new SpriteBatch();
-        texture = new Texture(Gdx.files.internal("background.jpg"));
-        texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-        sprite = new Sprite(texture);
-        sprite.setOrigin(0,0);
-        sprite.setPosition(-sprite.getWidth()/2,-sprite.getHeight()/2);
-        Gdx.input.setInputProcessor(new GestureDetector(this));
+        stage = new Stage();
+        MyActor myActor = new MyActor();
+        stage.addActor(myActor);
     }
 
     @Override
-    public void render(){
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+    public void dispose() {
+        stage.dispose();
+    }
+
+    @Override
+    public void render() {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
-        sprite.draw(batch);
-        batch.end();
+        stage.draw();
     }
 
     @Override
-    public boolean touchDown(float x, float y, int pointer, int button) {
-        return false;
+    public void resize(int width, int height) {
     }
 
     @Override
-    public boolean tap(float x, float y, int count, int button) {
-        return false;
+    public void pause() {
     }
 
     @Override
-    public boolean longPress(float x, float y) {
-        return false;
-    }
-
-    @Override
-    public boolean fling(float velocityX, float velocityY, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean pan(float x, float y, float deltaX, float deltaY) {
-        camera.translate(deltaX,0);
-        camera.update();
-        return false;
-    }
-
-    @Override
-    public boolean panStop(float x, float y, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean zoom(float initialDistance, float distance) {
-        return false;
-    }
-
-    @Override
-    public boolean pinch(Vector2 initialPointer1, Vector2 initialPointer2, Vector2 pointer1, Vector2 pointer2) {
-        return false;
-    }
-
-    @Override
-    public void pinchStop() {
-
-    }
-
-    @Override
-    public boolean keyDown(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyUp(int keycode) {
-        return false;
-    }
-
-    @Override
-    public boolean keyTyped(char character) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(int amount) {
-        return false;
+    public void resume() {
     }
 }
+
