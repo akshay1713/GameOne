8c5421d329f980d3b26fb81566e703400cb62f68
Akshay Singh <akshaysingh.1713@gmail.com>
2017-07-12 22:38:19 +0530

    Generate bullet at the correct position

--- core/src/com/mygdx/drop/Tank.java
+++ core/src/com/mygdx/drop/Tank.java
@@ -2,6 +2,7 @@ package com.mygdx.drop;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.utils.Align;
 
 /**
  * Created by akshaysingh on 09/07/17.
@@ -24,9 +25,17 @@ public class Tank extends Group{
         Vector2 touchVector = new Vector2(screenX, screenY);
         Vector2 xDiff = touchVector.sub(turretStageCoords);
         turret.setRotation(xDiff.angle() - 90);
-        System.out.println(turret.getX());
     }
 
     public void fire(float xPos, float yPos){
+        TankBullet bullet = new TankBullet(turret.getHeight());
+        bullet.setRotation(turret.getRotation());
+        Vector2 leftCoords = turret.localToParentCoordinates(new Vector2(turret.getX(Align.topLeft), turret.getY(Align.topLeft)));
+        Vector2 rightCoords = turret.localToParentCoordinates(new Vector2(turret.getX(Align.topRight), turret.getY(Align.topRight)));
+        bullet.setOrigin(turret.getOrigin().x, turret.getOrigin().y);
+        float moveX = (leftCoords.x + rightCoords.x)/2  - body.getWidth()/2;
+        float moveY = (rightCoords.y + leftCoords.y)/2 - body.getHeight()/2;
+        bullet.moveBy(moveX, moveY);
+        addActor(bullet);
     }
 }
