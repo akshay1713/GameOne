8dbc5984f9bb83fa5287c111fc924a7519da2aa3
Akshay Singh <akshaysingh.1713@gmail.com>
2017-07-17 20:44:42 +0530

    Refactoring

--- core/src/com/mygdx/drop/TankBullet.java
+++ core/src/com/mygdx/drop/TankBullet.java
@@ -6,7 +6,6 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 
 
 /**
@@ -15,7 +14,11 @@ import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
 public class TankBullet extends Actor{
     private TextureAtlas textureAtlas = new TextureAtlas(Gdx.files.internal("Spritesheet/tanksprite.atlas"));
     private TextureRegion region = textureAtlas.findRegion("bulletBeige");
-    private  float angle;
+    private float quadrantAngle;
+    private int quadrant;
+    private static float distance = 15f;
+    private static float width = Gdx.graphics.getWidth();
+    private static float height = Gdx.graphics.getHeight();
 
     public TankBullet(){
         setBounds(0,0,region.getRegionWidth(),region.getRegionHeight());
@@ -31,34 +34,14 @@ public class TankBullet extends Actor{
     private void setAngle(Vector2 clickVector) {
         Vector2 originInStage = localToStageCoordinates(new Vector2(getOriginX(), getOriginY()));
         Vector2 xDiff = clickVector.sub(originInStage);
-        this.angle = xDiff.angle();
+        float angle = xDiff.angle();
+        quadrantAngle = angle % 90;
+        quadrant = getQuadrant(angle);
+        if (quadrant == 2 || quadrant == 4){
+            quadrantAngle = 90 - quadrantAngle;
+        }
     }
 
-    private void setFireAction(){
-        MoveToAction fireAction = new MoveToAction();
-        Vector2 destination = getDestinationVector();
-        fireAction.setPosition(destination.x, destination.y);
-        fireAction.setDuration(2f);
-        addAction(fireAction);
-    }
-
-    //TODO: What the hell are you doing here? Use a proper way to get the destination vector
-    private Vector2 getDestinationVector(){
-        Vector2 destination = new Vector2();
-        float currentX = getX();
-        float currentY = getY();
-        destination.x = 2000;
-        destination.y = destination.x * currentY/currentX;
-        if(signShouldChange(currentX, currentY)){
-            destination.x = -destination.x;
-            destination.y = -destination.y;
-        }
-        destination = destination.rotate(60f);
-        if (destination.x < 0){
-            destination = destination.rotate(-15f);
-        }
-        return localToStageCoordinates(destination);
-    }
 
     private boolean signShouldChange(float positionX, float positionY){
         if(positionX < 0 || (positionY < 0 && positionX < 0)){
@@ -68,7 +51,6 @@ public class TankBullet extends Actor{
     }
 
     private void setInitialParameters(float angle, Vector2 origin, Vector2 position){
-//        origin = stageToLocalCoordinates(origin);
        setOrigin(origin.x, origin.y);
        setPosition(position.x, position.y);
        setRotation(angle);
@@ -78,6 +60,21 @@ public class TankBullet extends Actor{
         int angle2 = (int)angle;
         return (angle2/90)%4+1;
     }
+
+    private Vector2 getNextPosition(Vector2 current){
+        float deltaX = (float) Math.cos(Math.toRadians(quadrantAngle)) * distance;
+        float deltaY = (float) Math.sin(Math.toRadians(quadrantAngle)) * distance;
+        float x2 = current.x + deltaX;
+        float y2 = current.y + deltaY;
+        if (quadrant == 2 || quadrant == 3){
+            x2 = current.x - deltaX;
+        }
+        if (quadrant == 3 || quadrant == 4){
+            y2 = current.y - deltaY;
+        }
+        return new Vector2(x2, y2);
+    }
+
 
     @Override
     public void draw(Batch batch, float alpha){
@@ -89,28 +86,37 @@ public class TankBullet extends Actor{
     public void act(float delta){
         super.act(delta);
         Vector2 current = new Vector2(getX(), getY());
-        int quadrant = getQuadrant(angle);
-        double quadrantAngle = angle % 90;
-        if (quadrant == 2 || quadrant == 4){
-            quadrantAngle = 90 - quadrantAngle;
+        if(current.x < 0 || current.x > width || current.y < 0 || current.y > height){
+            remove();
+            return;
         }
-        float deltaDistance = 15;
-        float deltaX = (float) Math.cos(Math.toRadians(quadrantAngle)) * deltaDistance;
-        float deltaY = (float) Math.sin(Math.toRadians(quadrantAngle)) * deltaDistance;
-        float x2 = current.x + deltaX;
-        float y2 = current.y + deltaY;
-        if (quadrant == 2 || quadrant == 3){
-            x2 = current.x - deltaX;
-        }
-        if (quadrant == 3 || quadrant == 4){
-            y2 = current.y - deltaY;
-        }
-        setPosition(x2, y2);
-//        Array<Action> actions = getActions();
-//        if(actions.size == 0 ) {
-//           //The bullet has crossed the border, hehehe....
-//            this.remove();
-//            return;
-//        }
+        Vector2 nextPosition = getNextPosition(current);
+        setPosition(nextPosition.x, nextPosition.y);
     }
+
+    //TODO: Use this code and and action instead of manually updating the position
+//    private void setFireAction(){
+//        MoveToAction fireAction = new MoveToAction();
+//        Vector2 destination = getDestinationVector();
+//        fireAction.setPosition(destination.x, destination.y);
+//        fireAction.setDuration(2f);
+//        addAction(fireAction);
+//    }
+//
+//    private Vector2 getDestinationVector(){
+//        Vector2 destination = new Vector2();
+//        float currentX = getX();
+//        float currentY = getY();
+//        destination.x = 2000;
+//        destination.y = destination.x * currentY/currentX;
+//        if(signShouldChange(currentX, currentY)){
+//            destination.x = -destination.x;
+//            destination.y = -destination.y;
+//        }
+//        destination = destination.rotate(60f);
+//        if (destination.x < 0){
+//            destination = destination.rotate(-15f);
+//        }
+//        return localToStageCoordinates(destination);
+//    }
 }
